676e7f7385759e6256a55e7c8710ead08f8689cf
Taylor Allen Wilsie <42305227+Wilsieapps@users.noreply.github.com>
2020-05-25 11:02:42 -0400

    apple commit

--- brithday/lib/src/routes.dart
+++ brithday/lib/src/routes.dart
@@ -11,10 +11,8 @@ abstract class Routes {
     switch(settings.name){
       case '/':
       return MaterialPageRoute(builder: (context) => Landing());
-      break;
       case '/signup':
       return MaterialPageRoute(builder: (context) => Signup());
-      break;
       case '/login':
       return MaterialPageRoute(builder: (context) => Login());
       default:
@@ -26,10 +24,8 @@ abstract class Routes {
     switch(settings.name){
       case '/':
       return CupertinoPageRoute(builder: (context) => Landing());
-      break;
       case '/signup':
       return CupertinoPageRoute(builder: (context) => Signup());
-      break;
       case '/login':
       return CupertinoPageRoute(builder: (context) => Login());
       default:
